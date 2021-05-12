8ced9996ae083eceb7208d5798868fd514d72191
Nightstars <ChristZhangowner@gmail..com>
2021-05-12 13:09:07 +0800

    添加项目文件。

---
+++ SmartTools_CS/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using AduSkin.Controls.Metro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmartTools_CS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#52a9ff"));
+        }
+    }
+}
