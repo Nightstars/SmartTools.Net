32b3c4090d29d0f4ac6d9bba86a9fae7aa0b128c
Nightstars <ChristZhangowner@gmail..com>
2021-05-24 13:07:03 +0800

    add tab control

---
+++ SmartTools_CS/ViewModel/MainViewModel.cs
@@ -0,0 +1,103 @@
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using SmartTools_CS.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace SmartTools_CS.ViewModel
+{
+   public class MainViewModel : ViewModelBase
+   {
+      public MainViewModel()
+      {
+
+      }
+
+
+      private int _SelectedModularIndex;
+      /// <summary>
+      /// 属性.
+      /// </summary>
+      public int SelectedModularIndex
+      {
+         get { return _SelectedModularIndex; }
+         set { 
+            Set(ref _SelectedModularIndex, value);
+            if (value == 2)
+               MainBackground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+            else if (value == 3)
+               MainBackground = new SolidColorBrush(Color.FromRgb(250, 251, 252));
+            else
+               MainBackground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+         }
+      }
+
+      private SolidColorBrush _MainBackground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+      /// <summary>
+      /// 属性.
+      /// </summary>
+      public SolidColorBrush MainBackground
+      {
+         get { return _MainBackground; }
+         set { Set(ref _MainBackground, value); }
+      }
+      /// <summary>
+      /// 命令Command
+      /// </summary>
+      public ICommand OpenClick => new RelayCommand<string>((e) =>
+      {
+          switch (e)
+          {
+              case "AduSkinDemo":
+                  //new UserControl1().Show();
+                  return;
+              default:
+                  break;
+          }
+      });
+
+        /// <summary>
+        /// 常见控件
+        /// </summary>
+        private UserControl _CommonControlCase = new UserControl1();
+        public UserControl CommonControlCase
+        {
+            get { return _CommonControlCase; }
+            set { Set(ref _CommonControlCase, value); }
+        }
+        /// <summary>
+        /// 实用案例
+        /// </summary>
+        private UserControl _PracticalCase = new UserControl1();
+        public UserControl PracticalCase
+        {
+            get { return _PracticalCase; }
+            set { Set(ref _PracticalCase, value); }
+        }
+        /// <summary>
+        /// 关于
+        /// </summary>
+        private UserControl _AduSkinAbout = new UserControl1();
+        public UserControl AduSkinAbout
+        {
+            get { return _AduSkinAbout; }
+            set { Set(ref _AduSkinAbout, value); }
+        }
+        /// <summary>
+        /// 支持与赞助
+        /// </summary>
+        private UserControl _AduSkinSupport = new UserControl1();
+        public UserControl AduSkinSupport
+        {
+            get { return _AduSkinSupport; }
+            set { Set(ref _AduSkinSupport, value); }
+        }
+
+    }
+}
