8f0740e44bd1175b590d5d157218ec2d7f090bd5
Nightstars <ChristZhangowner@gmail..com>
2021-10-29 17:09:41 +0800

    new features

---
+++ SmartSoft.GetVersion/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SmartSoft.GetVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileVersionInfo fileversioninfo = FileVersionInfo.GetVersionInfo(args[0]);
+            Console.WriteLine(fileversioninfo.ProductVersion);
+            Console.ReadKey();
+        }
+    }
+}
--- publish.bat
+++ publish.bat
@@ -1,6 +1,11 @@
+::创建发布文件夹
 md update
+
+::复制构建的文件到新文件夹中
 echo copy new file
 Xcopy .\build\Release .\update /E/H/C/I
+
+::删除无用文件
 echo delete ignore file
 del .\update\SmartTools.Net.runtimeconfig.dev.json
 rmdir /s/q .\update\ca-ES
@@ -12,8 +17,17 @@ rmdir /s/q .\update\pl
 rmdir /s/q .\update\pt-BR
 rmdir /s/q .\update\ru
 rmdir /s/q .\update\tr
-del .\update\Update.*
+del .\update\Update.runtimeconfig.dev.json
+
+::删除旧的zip
 del *.zip
 echo complete successfully
+
+::压缩发布文件夹
 7z a update.zip .\update\**
+
+::删除发布文件夹
 rmdir /s/q update
+
+::获取本次发布的版本信息
+.\SmartSoft.GetVersion\bin\Release\net5.0\SmartSoft.GetVersion.exe .\build\Release\SmartTools.Net.exe
